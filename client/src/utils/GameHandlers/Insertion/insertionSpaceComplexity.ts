f8bc1a7648102d8170a9ffe04328f52468a98b64
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-16 19:15:09 -0700

    Create insertionSpaceComplexity.ts

---
+++ client/src/utils/GameHandlers/Insertion/insertionSpaceComplexity.ts
@@ -0,0 +1,22 @@
+import { shuffle } from "d3-array";
+
+function insertionSpaceComplexity() {
+  const rightAnswer = "1";
+  const wrongAnswers = shuffle(["nlogn", "n", "logn"]);
+
+  const answerChoices = [{ isCorrect: true, answerContent: rightAnswer }];
+
+  for (const wrongChoice of wrongAnswers) {
+    answerChoices.push({ isCorrect: false, answerContent: wrongChoice });
+  }
+
+  const gameDisplayObject = {
+    answerChoices,
+    content: "Insertion Sort",
+    question: "What is the space complexity of the algorithm below?",
+  };
+
+  return gameDisplayObject;
+}
+
+export default insertionSpaceComplexity;
