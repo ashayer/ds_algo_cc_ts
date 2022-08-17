212da8d8bcbe0fa4a88d4f32fa58d064393ad0c4
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-16 19:29:46 -0700

    added css for array bars content

--- client/src/components/GameQuestionContent/GameQuestionContent.tsx
+++ client/src/components/GameQuestionContent/GameQuestionContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Grid, Box, Typography } from "@mui/material";
 
 const DragContent = ({ questionContent }: any) => {
   return (
@@ -9,12 +9,49 @@ const DragContent = ({ questionContent }: any) => {
     </div>
   );
 };
+const ArrayBarsContent = ({ questionContent }: any) => {
+  return (
+    <Grid container sx={{ position: "relative", justifyContent: "center" }}>
+      {questionContent.map((value: number) => (
+        <Grid item key={value} sx={{ height: "25vh", width: "8vw" }} md={1} sm={0}>
+          <Box
+            sx={{
+              height: `${value * 3}vh`,
+              backgroundColor: "#036bfc",
+              color: "white",
+              position: "absolute",
+              bottom: "0",
+              borderRadius: "5px 5px 0px 0px",
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="h4" sx={{ width: "4vw" }}>{`${value}`}</Typography>
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 const GameQuestionContent = ({ questionInfo, questionContent }: any) => {
-  if (questionInfo.qType === "DRAG-CODE" || questionInfo.qType === "DRAG-ARRAY-BARS") {
-    return <DragContent questionContent={questionContent} />;
+  switch (questionInfo.qType) {
+    case "STATE-AFTER-SWAPS":
+      return <ArrayBarsContent questionContent={questionContent} />;
+    case "TIME-COMPLEXITY-DIRECT":
+      return <div>{questionContent}</div>;
+    case "SPACE-COMPLEXITY":
+      return <div>{questionContent}</div>;
+    case "CHOOSE-PSEUDOCODE":
+      return <div>{questionContent}</div>;
+    case "TIME-COMPLEXITY-FROM-ARRAY":
+      return <div>{questionContent}</div>;
+    case "DRAG-CODE":
+      return <DragContent questionContent={questionContent} />;
+    case "DRAG-ARRAY-BARS":
+      return <DragContent questionContent={questionContent} />;
+    default:
+      return <div>Error</div>;
   }
-  return <div>{questionContent}</div>;
 };
 
 export default GameQuestionContent;
